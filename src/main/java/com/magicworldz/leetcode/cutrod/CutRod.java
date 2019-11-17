54ae41ce3c89d70a3a63dbc464e356e16651de22
magicworldz <zhaojunlucky@sina.com>
2019-11-17 15:07:02 +0800

    update cut rod to print the cuts

--- src/main/java/com/magicworldz/leetcode/cutrod/CutRod.java
+++ src/main/java/com/magicworldz/leetcode/cutrod/CutRod.java
@@ -3,10 +3,7 @@ package com.magicworldz.leetcode.cutrod;
 import com.magicworldz.leetcode.common.duration.Duration;
 import com.magicworldz.leetcode.common.duration.LeetCode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CutRod {
     public static void main(String[] args) {
@@ -15,6 +12,7 @@ public class CutRod {
         int[] prices = new int[] {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
         List<Integer> cuttings = new ArrayList<>();
         System.out.println("Max price: " + app.cutRod(7, lens, prices, cuttings));
+        Collections.sort(cuttings);
         System.out.println("cuttings: " + cuttings.toString());
     }
 
@@ -26,14 +24,47 @@ public class CutRod {
             lenPriceMap.put(lens[i], prices[i]);
         }
         int[] dp = new int[n + 1];
-
+        int[][] b = new int[2][n + 1];
         for (int i = 1; i <= n; ++i) {
             dp[i] = getLenPrice(lenPriceMap, i);
+            b[0][i] = b[1][i] = i;
             for (int j = 1; j <= i; ++j) {
-                dp[i] = Math.max(dp[i], dp[j] + dp[i-j]);
+                int p = dp[j] + dp[i-j];
+                if (p > dp[i]) {
+                    dp[i] = p;
+                    b[0][i] = i - j;
+                    b[1][i] = j;
+                }
             }
         }
+
+        calcCuts(n, b, cuttings);
         return dp[n];
+    }
+
+    private void calcCuts(int n, int[][] b, List<Integer> cuttings) {
+        int i;
+        int j;
+        int k;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(n);
+        while (!queue.isEmpty() && n > 0) {
+            k = queue.poll();
+            i = b[0][k];
+            j = b[1][k];
+            if (i == j) {
+                cuttings.add(i);
+                queue.add(i);
+                n -= i;
+            } else {
+                cuttings.add(i);
+                cuttings.add(j);
+                queue.add(i);
+                queue.add(j);
+                n -= i;
+                n -= j;
+            }
+        }
     }
 
     private int getLenPrice(Map<Integer, Integer> lenPriceMap, int len) {
